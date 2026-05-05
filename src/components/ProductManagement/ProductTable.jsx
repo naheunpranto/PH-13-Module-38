e5b752449761894e773bce47ab3064773863f6a2
Naheun Alam <naheunpranto258@gmail.com>
2026-05-05 22:45:40 +0600

    Display form data in a table and form error handle

--- src/components/ProductManagement/ProductTable.jsx
+++ src/components/ProductManagement/ProductTable.jsx
@@ -1,9 +1,31 @@
  
 
-const ProductTable = () => {
+const ProductTable = ({products}) => {
     return (
         <div>
-            
+            <h3>Products: {products.length}</h3>
+            <table>
+                <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>Product</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        products.map((product, index) => 
+                            <tr key={index}>
+                                <td>{index + 1}</td>
+                                <td>{product.name}</td>
+                                <td>{product.price}</td>
+                                <td>{product.quantity}</td>
+                            </tr>
+                        )
+                    }
+                </tbody>
+            </table>
         </div>
     );
 };
